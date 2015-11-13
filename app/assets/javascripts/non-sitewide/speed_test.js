3c8e0a876b378da4affd2643e17a8c6e2f1d68ff
Marcus Ingelborn <MrIngelborn@pub93-132.pub.ltu.se>
2015-11-13 10:34:09 +0100

    Now waits until fully loaded until downloading file

--- app/assets/javascripts/non-sitewide/speed_test.js
+++ app/assets/javascripts/non-sitewide/speed_test.js
@@ -2,33 +2,45 @@ function test_speed() {
 	//Size of target
 	var size = 10; //Megabyte
 	
-	$.ajax({
-	    beforeSend: function(){
-		    // set the start time
-	        window.startTime = new Date();
-	    },
-	    url: '/assets/10M.dat',
-	    success: function( data ){
-		    // set the end time
-	        window.endTime = new Date();
-			
-			// calculate and print the download speed
-	        $("#speed").append("Down speed: " + (size*8)/((window.endTime - window.startTime)/1000) + " Mb/s<br/>");
-	        
-	        // set an new start time
-	        window.startTime = new Date();
-	        
-	        // Send back the same piece of data to the server
-	        $.post(
-	        	'/pages/test',
-	        	data,
-	        	function() {
-			        // set the end time
-					window.endTime = new Date();
-					
-					// calculate and print the upload speed
-					$("#speed").append("Up speed: " + (size*8)/((window.endTime - window.startTime)/1000) + " Mb/s");
-	        })
-	    }
+	$( window ).load( function() {	
+		// Download test
+		$.ajax({
+		    beforeSend: function(){
+			    // set the start time
+		        window.startTime = new Date();
+		    },
+		    url: '/assets/10M.dat',
+		    cache:false,
+		    success: function( file ){
+				// set the end time
+				window.endTime = new Date();
+				
+				// calculate and print the download speed
+				$("#speed").append("Down speed: " + (size*8)/((window.endTime - window.startTime)/1000) + " Mb/s<br/>");
+				
+				// Upload test
+			    
+			    // Send back the same piece of data to the server
+				$.ajax({
+				    method:'POST',
+				    beforeSend: function() {
+					    // set an new start time
+					    window.startTime = new Date();
+				    },
+				    url: '/pages/test',
+				    cache:false,
+				    data: {
+					    data: file
+				    },
+				    success: function() {
+					    // set the end time
+						window.endTime = new Date();
+						
+						// calculate and print the upload speed
+						$("#speed").append("Up speed: " + (size*8)/((window.endTime - window.startTime)/1000) + " Mb/s");
+				    } 
+				});
+		    }
+		});
 	});
 }
