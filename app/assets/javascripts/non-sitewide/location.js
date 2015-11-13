3e7bef7040dd8ddcee1620f0b7f3f21c7d5c2d84
Marcus Ingelborn <MrIngelborn@pub93-132.pub.ltu.se>
2015-11-13 12:35:12 +0100

    Merged the two functions into one

--- app/assets/javascripts/non-sitewide/location.js
+++ app/assets/javascripts/non-sitewide/location.js
@@ -1,11 +1,10 @@
 function getLocation() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(onSuccess);
+        navigator.geolocation.getCurrentPosition(function(position) {
+	        this.coords = position.coords;
+        });
     } else {
         console.log("Error: Geolocation is not supported by this browser.");
         return false;
     }
-}
-function onSuccess(position) {
-	this.coords = position.coords;
 }
