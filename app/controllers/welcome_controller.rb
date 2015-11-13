944d16d55b85c48df103a19cca77455212fe3178
Marcus Ingelborn <MrIngelborn@pub93-132.pub.ltu.se>
2015-11-13 10:33:06 +0100

    Returning size of the sent object

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,12 @@
 class WelcomeController < ApplicationController
-  def index
-  end
-  
-  def test
-	  
-  end
+	def index
+	end
+	
+	def test
+	end
+	
+	def test_post
+		data = params[:data]
+		render :text => data.length, :layout => false
+	end
 end
