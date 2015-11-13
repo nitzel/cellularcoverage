b403126e9d5942d602ab9f74f8b2439a7ae2d8ff
Marcus Ingelborn <MrIngelborn@Marcuss-MacBook-Pro.local>
2015-11-13 07:20:24 +0100

    Bugfix for #5

--- app/assets/javascripts/non-sitewide/leaflet_map.js
+++ app/assets/javascripts/non-sitewide/leaflet_map.js
@@ -1,76 +1,74 @@
-$("#map").ready( function() {
-	// the base layers
-	var baseMaps = {
-		//OSM
-		"OSM" : L.tileLayer(
-			'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', 
-			{
-				maxZoom: 19,
-				attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-			}
-		),
-	};
-	// the overlay layers
-	var overlayMaps = {
-		// testone
-		"testone" : L.tileLayer.wms(
-			"http://map.mringelborn.com:8080/geoserver/wms", 
-			{
-				layers: 'test:testone',
-				format: 'image/png',
-				transparent: true,
-				version: '1.1.0',
-				opacity: 0.5,
-			}
-		),
-	}	
-	var map = L.map(
-		'map', 
+// the base layers
+var baseMaps = {
+	//OSM
+	"OSM" : L.tileLayer(
+		'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', 
 		{
-			center: {
-				lat: 65.61406803735737, 
-				lng: 22.17461585998535
-			},
-			zoom: 5,
-			maxZoom: 15,
-			layers: [
-				baseMaps["OSM"],
-				overlayMaps["testone"]
-			],
+			maxZoom: 19,
+			attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 		}
-	);
-	
-	L.control.layers(baseMaps, overlayMaps).addTo(map);
-	
-	// Marker & Popupt for visualization of geolocation
-	var locationMarker = L.marker().bindPopup('This is you!');
-	
-	// Clicking the locate/stop locating button
-	function locate(btn) {
-		if(btn.textContent=='Locate'){
-			// http://leafletjs.com/reference.html#map-locate-options
-			map.locate({
-				watch:true,
-				maximumAge:2000,
-				enableHighAccuracy:true,
-				setView:true
-			});
-			btn.textContent = 'Stop locating';
-		} else {
-			map.stopLocate();
-			map.removeLayer(locationMarker);
-			btn.textContent='Locate'
+	),
+};
+// the overlay layers
+var overlayMaps = {
+	// testone
+	"testone" : L.tileLayer.wms(
+		"http://map.mringelborn.com:8080/geoserver/wms", 
+		{
+			layers: 'test:testone',
+			format: 'image/png',
+			transparent: true,
+			version: '1.1.0',
+			opacity: 0.5,
 		}
+	),
+}	
+var map = L.map(
+	'map', 
+	{
+		center: {
+			lat: 65.61406803735737, 
+			lng: 22.17461585998535
+		},
+		zoom: 5,
+		maxZoom: 15,
+		layers: [
+			baseMaps["OSM"],
+			overlayMaps["testone"]
+		],
 	}
-	// when the geolocation was found
-	// http://leafletjs.com/reference.html#location-event
-	map.on('locationfound',function(ev){ 
-		locationMarker.setLatLng(ev.latlng).addTo(map);
-	});	   
-	
-	// if geolocation could not be found
-	// http://leafletjs.com/reference.html#location-event
-	map.on('locationerror',function(ev){
-		btn.textContent = "does not work: "+ev.message;
-	});
+);
+
+L.control.layers(baseMaps, overlayMaps).addTo(map);
+
+// Marker & Popupt for visualization of geolocation
+var locationMarker = L.marker().bindPopup('This is you!');
+
+// Clicking the locate/stop locating button
+function locate(btn) {
+	if(btn.textContent=='Locate'){
+		// http://leafletjs.com/reference.html#map-locate-options
+		map.locate({
+			watch:true,
+			maximumAge:2000,
+			enableHighAccuracy:true,
+			setView:true
+		});
+		btn.textContent = 'Stop locating';
+	} else {
+		map.stopLocate();
+		map.removeLayer(locationMarker);
+		btn.textContent='Locate'
+	}
+}
+// when the geolocation was found
+// http://leafletjs.com/reference.html#location-event
+map.on('locationfound',function(ev){ 
+	locationMarker.setLatLng(ev.latlng).addTo(map);
+});	   
+
+// if geolocation could not be found
+// http://leafletjs.com/reference.html#location-event
+map.on('locationerror',function(ev){
+	btn.textContent = "does not work: "+ev.message;
 });
