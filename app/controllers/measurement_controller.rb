ce0f601f19f83fedbf1a49f9822c1b9c4b31b832
Marcus Ingelborn <MrIngelborn@Marcuss-MacBook-Pro.local>
2015-11-17 17:51:27 +0100

    Added creation of measurement in database on submisson

--- app/controllers/measurement_controller.rb
+++ app/controllers/measurement_controller.rb
@@ -3,6 +3,12 @@ class MeasurementController < ApplicationController
 	end
 	
 	def create
-		render plain: params[:measurement].inspect
+		measurement = Measurement.create(measurement_params)
+		render plain: measurement
 	end
+	
+	private
+		def measurement_params
+			params.require(:measurement).permit(:latitude, :longitude, :quality)
+		end
 end
