6239ca8bd4c315d9096022be67012c6d3615285f
Marcus Ingelborn <mringelborn@gmail.com>
2015-11-30 17:19:55 +0100

    Added requirement for presence for both lat, long and quality on creation of new measurement

--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -1,5 +1,5 @@
 class Measurement < ActiveRecord::Base
-	validates :latitude, :longitude, :quality, numericality: true
+	validates :latitude, :longitude, :quality, numericality: true, presence: true
 	validates :quality, inclusion: 0..10
 	validates :latitude, inclusion: -90..90
 	validates :longitude, inclusion: -180..180
