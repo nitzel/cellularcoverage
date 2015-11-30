4c0814b485110a6e5cd9848109a0e2d39c19a614
Marcus Ingelborn <mringelborn@gmail.com>
2015-11-30 16:49:16 +0100

    Added valudation in numerical range for lat, lon and quality

--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -1,3 +1,6 @@
 class Measurement < ActiveRecord::Base
 	validates :latitude, :longitude, :quality, numericality: true
+	validates :quality, inclusion: 0..10
+	validates :latitude, inclusion: -90..90
+	validates :longitude, inclusion: -180..180
 end
