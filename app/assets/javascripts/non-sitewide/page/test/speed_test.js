2ff4e2022e25ab6608f17ec1380b39c8dec8eb2f
Marcus Ingelborn <mringelborn@gmail.com>
2015-11-30 15:57:55 +0100

    Added post of test results on finished test

--- app/assets/javascripts/non-sitewide/page/test/speed_test.js
+++ app/assets/javascripts/non-sitewide/page/test/speed_test.js
@@ -1,6 +1,6 @@
 SomApi.account = "SOM5645ad2eec3af"; //your API Key here
 SomApi.domainName = "marcus.mringelborn.com"; //your domain or sub-domain here
-SomApi.config.sustainTime = 4;
+SomApi.config.sustainTime = 5;
 SomApi.config.userInfoEnabled = false;
 SomApi.config.progress.verbose = true;
 
@@ -9,15 +9,18 @@ SomApi.onTestCompleted = function(testResult) {
 	$('#output').append("Upload: " +testResult.upload +"Mbps <br/>");
 	$('#output').append("Latency: " +testResult.latency +"ms <br/>");
 	$('#output').append("Jitter: " +testResult.jitter +"ms <br/>");
-	$('#output').append("Test Server: "+testResult.testServer +"<br/>");
-	$('#output').append("IP: " +testResult.ip_address +"<br/>");
-	$('#output').append("Hostname: " +testResult.hostname +"<br/>");
+	//$('#output').append("Test Server: "+testResult.testServer +"<br/>");
+	//$('#output').append("IP: " +testResult.ip_address +"<br/>");
+	//$('#output').append("Hostname: " +testResult.hostname +"<br/>");
+	
+	post_data(testResult);
 	
 	if (this.geolocation_enabled) {
-		console.log(this.coords);
+		
+		//console.log(this.coords);
 		delete(this.coords);
 	}
-	console.log(testResult);
+	//console.log(testResult);
 };
 
 SomApi.onProgress = function(progress) {
@@ -32,17 +35,6 @@ SomApi.onError = function(error) {
 };
 
 function test_speed() {
-	geolocation_enabled = false;
-	if(navigator.connection && navigator.connection.type == 'cellular') {
-		if (navigator.geolocation) {
-			geolocation_enabled = true;
-		} else {
-			$("#info").html('Geolocation is not supported by this browser. You are not able to contribute.<br/>');
-		}
-	}
-	else {
-		$("#info").html('You need to be on a cellular network to be able to contribute.<br/>');
-	}
 	if (geolocation_enabled) {
 		console.log("Getting geolocation");
 		if (!getLocation()) {
@@ -52,4 +44,57 @@ function test_speed() {
 	}
 	$("#info").append("Starting test");
 	SomApi.startTest();
+}
+
+function on_load() {
+	// St to true by deafukt for testing purposes
+	this.geolocation_enabled = true;
+	if(navigator.connection && navigator.connection.type == 'cellular') {
+		if (navigator.geolocation) {
+			this.geolocation_enabled = true;
+		} else {
+			$("#info").html('Geolocation is not supported by this browser. You are not able to contribute.<br/>');
+		}
+	}
+	else {
+		$("#info").html('You need to be on a cellular network to be able to contribute.<br/>');
+	}
+}
+
+function post_data(testResult) {
+	console.log("post_data()");
+	if(testResult == undefined) {
+		console.log(testResult);
+		return false;
+	}
+	
+	max_up = 100;
+	max_down = 100;
+	
+	quality = (testResult.upload/max_up + testResult.download/max_down) / 2;
+	
+	// To make sure: 0 ≤ quality ≤ 10
+	if (quality > 10) {
+		quality = 10;
+	}
+	else if (quality < 0) {
+		quality = 0;
+	}
+	
+	console.log("Quality: " + quality);
+	
+	measurement = {
+		latitude: this.coords.latitude,
+		longitude: this.coords.longitude,
+		quality: quality
+	};
+	
+	$.post("/measurement",
+	{
+		measurement: measurement
+	},
+	function( data ) {
+		//on success
+		console.log(data);
+	});
 }
