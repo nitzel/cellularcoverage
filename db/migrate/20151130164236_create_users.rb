26d6121b12de1632c3e914e3d1af8e70cbeb8001
Marcus Ingelborn <mringelborn@gmail.com>
2015-12-09 07:51:46 +0100

    Fixed database, removed old oauth-stuff and made it run somehow

--- db/migrate/20151130164236_create_users.rb
+++
@@ -1,9 +0,0 @@
-class CreateUsers < ActiveRecord::Migration
-  def change
-    create_table :users do |t|
-      t.string :email_address, null: false, index: true
-	  
-      t.column :created_at,  :datetime, null: false
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151130164236) do
+ActiveRecord::Schema.define(version: 20151130161254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,12 +22,5 @@ ActiveRecord::Schema.define(version: 20151130164236) do
     t.float    "quality",    null: false
     t.datetime "created_at", null: false
   end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "email_address", null: false
-    t.datetime "created_at",    null: false
-  end
-
-  add_index "users", ["email_address"], name: "index_users_on_email_address", using: :btree
 
 end
